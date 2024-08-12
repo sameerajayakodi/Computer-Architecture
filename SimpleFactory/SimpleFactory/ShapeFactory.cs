28aad5864920f2120257fcbfe30c2dd4b64348c2
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-12 15:26:59 +0530

    Create ShapeFactory.cs

---
+++ SimpleFactory/SimpleFactory/ShapeFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class ShapeFactory
+    {
+        public Shape GetShape(string shapeType)
+        {
+            if (shapeType == null) {
+                return null;
+            }else if (shapeType == "circle")
+            {
+                return new Circle();
+            }else if(shapeType == "rectangle")
+            {
+                return new Rectangle(); 
+            }else if(shapeType == "squre")
+            {
+                return new Squre;
+            }
+        }
+    }
+}
