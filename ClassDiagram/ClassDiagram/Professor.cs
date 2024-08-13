4fef3a2f7c6b8b7f3fae3746fbf3e7ee853776ce
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 12:49:50 +0530

    Update Professor.cs

--- ClassDiagram/ClassDiagram/Professor.cs
+++ ClassDiagram/ClassDiagram/Professor.cs
@@ -8,5 +8,32 @@ namespace ClassDiagram
 {
     public class Professor
     {
+
+       public string ProfessorId {  get; set; }
+       public string Name { get; set; }
+       public string Specialization {  get; set; }
+        private List<Course> Courses;
+
+        public Professor(string professorId , string name , string specialization) {
+            
+            ProfessorId = professorId;
+
+            Name = name;
+            Specialization = specialization;
+            Courses = new List<Course>();
+
+        }
+
+        public void AddCourse(Course course) 
+        {
+            Courses.Add(course);
+        }
+
+        public List<Course> GetCourses()
+        {
+            return Courses; 
+        }
+
+
     }
 }
