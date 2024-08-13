47f3cc3fc49d423fdb73bb491b6a3dd88adda5c4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 11:22:35 +0530

    update

---
+++ ClassDiagram/ClassDiagram/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagram
+{
+    public class Department
+    {
+        string Name { get;,set; }
+        string Location { get; set; }
+
+    }
+}
