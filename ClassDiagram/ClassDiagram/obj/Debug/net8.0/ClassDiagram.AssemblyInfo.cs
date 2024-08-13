23801fabab45cb344af39821d09d72811bbf10df
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 13:46:19 +0530

    Update ClassDiagram.AssemblyInfo.cs

--- ClassDiagram/ClassDiagram/obj/Debug/net8.0/ClassDiagram.AssemblyInfo.cs
+++ ClassDiagram/ClassDiagram/obj/Debug/net8.0/ClassDiagram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassDiagram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f3cc3fc49d423fdb73bb491b6a3dd88adda5c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec5ac0f730bc8a0e5ecd1d704a182308424be3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassDiagram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassDiagram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
