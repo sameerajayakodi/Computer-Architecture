32abb8395795792d553f236994bd3d51c2633603
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 12:49:48 +0530

    Update Student.cs

--- ClassDiagram/ClassDiagram/Student.cs
+++ ClassDiagram/ClassDiagram/Student.cs
@@ -8,5 +8,26 @@ namespace ClassDiagram
 {
     public class Student
     {
+        public string StudentId {  get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+
+        private List<Professor> Professors { get; set; }
+
+        public Student(string studentId,string name , string major) {
+            StudentId = studentId;
+            Name = name;
+            Major = major;
+            Professors = new List<Professor>();
+        }
+
+        public void AddProfessor(Professor professor) { 
+                Professors.Add(professor);
+        }
+
+        public List<Professor> GetProfessors() { 
+          return Professors;
+        }
+
     }
 }
