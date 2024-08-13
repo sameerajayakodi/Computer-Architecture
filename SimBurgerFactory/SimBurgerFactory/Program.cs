a0732f3483e8dafe7710005b625580181f244c1c
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 10:28:59 +0530

    Update Program.cs

--- SimBurgerFactory/SimBurgerFactory/Program.cs
+++ SimBurgerFactory/SimBurgerFactory/Program.cs
@@ -5,13 +5,9 @@ class Progaram
     public static void Main(string[] args)
     {
         BurgerFactory burgerFactory = new BurgerFactory();
-         Burger burger1 = burgerFactory.OrderBurger("veggie");
-        burger1.MakeBurger();
-
-        Burger burger2 = burgerFactory.OrderBurger("chicken");
-        burger2.MakeBurger();
-
-        Burger burger3 = burgerFactory.OrderBurger("cheese");
-        burger3.MakeBurger();
+        burgerFactory.OrderBurger("veggie");
+        burgerFactory.OrderBurger("chicken");
+        burgerFactory.OrderBurger("cheese");
+        
     }
 }
