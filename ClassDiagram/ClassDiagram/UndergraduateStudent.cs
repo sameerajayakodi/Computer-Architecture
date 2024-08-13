c7f5e1f4d448dcf706bf00961200757f5c751be1
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 13:46:16 +0530

    Create UndergraduateStudent.cs

---
+++ ClassDiagram/ClassDiagram/UndergraduateStudent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagram
+{
+    public class UndergraduateStudent:Student
+    {
+        public int Year { get; set; }
+
+        public UndergraduateStudent(string studentId, string name, string major,int year)
+            : base(studentId, name, major)
+        { 
+         
+
+            Year = year;
+        }
+
+        public void DeclareMajor(string major)
+        {
+            Major = major;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $",Year : {Year}";
+        }
+    }
+}
