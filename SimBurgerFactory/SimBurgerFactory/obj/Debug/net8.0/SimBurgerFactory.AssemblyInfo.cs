2410031143de5887bfae832969df82ecf2751d95
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 10:29:16 +0530

    Update Project

--- SimBurgerFactory/SimBurgerFactory/obj/Debug/net8.0/SimBurgerFactory.AssemblyInfo.cs
+++ SimBurgerFactory/SimBurgerFactory/obj/Debug/net8.0/SimBurgerFactory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimBurgerFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3344b6cd9ad3ff2cdabeebc99522eb3fe87a067")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931161da4dfb96b993fa3f0d86c8c9bdb3b22805")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimBurgerFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimBurgerFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
