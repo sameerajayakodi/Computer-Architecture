125a8c6e1c73b94acdc2e78588eb8eddabcca9f4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 10:16:52 +0530

    Create BurgerFactory.cs

---
+++ SimBurgerFactory/SimBurgerFactory/BurgerFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimBurgerFactory
+{
+    public class BurgerFactory
+    {
+        public Burger OrderBurger(string burgerType) {
+           if(burgerType == null)
+            {
+                return null;
+            }else if(burgerType == "chicken")
+            {
+                return new ChickenBurger();
+            }else if (burgerType == "cheese")
+            {
+                return new CheeseBurger();
+            }else if (burgerType == "veggie")
+            {
+                return new VeggieBurger();  
+            }
+            return null;
+        }
+    }
+}
