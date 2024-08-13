02b66a4e329b381fbe075ee23d6b821fe885c7fc
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 10:21:46 +0530

    Update Program.cs

--- SimBurgerFactory/SimBurgerFactory/Program.cs
+++ SimBurgerFactory/SimBurgerFactory/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SimBurgerFactory;
+
+class Progaram
+{
+    public static void Main(string[] args)
+    {
+        BurgerFactory burgerFactory = new BurgerFactory();
+         Burger burger1 = burgerFactory.OrderBurger("veggie");
+        burger1.MakeBurger();
+
+        Burger burger2 = burgerFactory.OrderBurger("chicken");
+        burger2.MakeBurger();
+
+        Burger burger3 = burgerFactory.OrderBurger("cheese");
+        burger3.MakeBurger();
+    }
+}
