e3344b6cd9ad3ff2cdabeebc99522eb3fe87a067
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 10:17:02 +0530

    Create VeggieBurger.cs

---
+++ SimBurgerFactory/SimBurgerFactory/VeggieBurger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimBurgerFactory
+{
+    public class VeggieBurger:Burger
+    {
+        public override void MakeBurger()
+        {
+            Console.WriteLine("I am Making Veggie Burger...");
+        }
+    }
+}
