d0501b572997f25941f648a147703c16d3faa354
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 13:46:21 +0530

    Create GraduateStudent.cs

---
+++ ClassDiagram/ClassDiagram/GraduateStudent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagram
+{
+    public class GraduateStudent:Student
+    {
+        public string ReasearchTopic { get; set; }
+        public GraduateStudent(string studentId, string name, string major, string researchTopic)
+            :base(studentId,name,major) {
+
+            ReasearchTopic = researchTopic;
+        }
+
+        public void ApplyForTA()
+        {
+            Console.WriteLine("Apply for TA");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $",ReasearchTopic {ReasearchTopic}";
+        }
+    }
+}
