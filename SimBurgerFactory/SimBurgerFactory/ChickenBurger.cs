450ef6fd1b211a611e9924ee11600951a281c4b7
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-13 10:16:55 +0530

    Create ChickenBurger.cs

---
+++ SimBurgerFactory/SimBurgerFactory/ChickenBurger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimBurgerFactory
+{
+    public class ChickenBurger:Burger
+    {
+        public override void MakeBurger()
+        {
+            Console.WriteLine("I am Making Chicken Burger...");
+        }
+    }
+}
