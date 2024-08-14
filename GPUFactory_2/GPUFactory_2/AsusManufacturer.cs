fb57bfebc32bd675afa723bbb01bc584b9d7cf78
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-14 22:10:19 +0530

    Create AsusManufacturer.cs

---
+++ GPUFactory_2/GPUFactory_2/AsusManufacturer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPUFactory_2
+{
+    public class AsusManufacturer:Company
+    {
+
+        public Gpu AssembleGpu()
+        {
+            Gpu gpu = CreateGpu();
+            gpu.Assemble();
+            return gpu;
+        }
+        public override Gpu CreateGpu()
+        {
+            return new AsusGpu();
+        }
+    }
+}
