adbfbf4338adaf18c27b750d066e258d8a424cfe
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-14 22:10:35 +0530

    update Program

--- GPUFactory_2/GPUFactory_2/obj/Debug/net8.0/GPUFactory_2.AssemblyInfo.cs
+++ GPUFactory_2/GPUFactory_2/obj/Debug/net8.0/GPUFactory_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GPUFactory_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d041965c7fadefa41537e6fa668bbe89ebfda5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8331f553c8cc7a49dd552de991b313aa2f131cb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GPUFactory_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GPUFactory_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
