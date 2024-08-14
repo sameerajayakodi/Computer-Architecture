7319a95a8484308e2544d55e00ec0b3eff4c412d
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-14 22:00:19 +0530

    Create AsusGpu.cs

---
+++ GPUFactory_2/GPUFactory_2/AsusGpu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPUFactory_2
+{
+    public class AsusGpu:Gpu
+    {
+        public void Assemble()
+        {
+            Console.WriteLine("ASUS GPU Assemble...");
+        }
+    }
+}
