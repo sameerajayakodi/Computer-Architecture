0d041965c7fadefa41537e6fa668bbe89ebfda5a
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-14 22:01:23 +0530

    Create Company.cs

---
+++ GPUFactory_2/GPUFactory_2/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPUFactory_2
+{
+    public abstract class Company
+    {
+
+        public abstract Gpu CreateGpu();
+    }
+}
