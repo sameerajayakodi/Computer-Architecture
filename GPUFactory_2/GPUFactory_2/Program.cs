90f37f751d50c9726edd12c712e88a0f059d1574
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-14 22:10:24 +0530

    Update Program.cs

--- GPUFactory_2/GPUFactory_2/Program.cs
+++ GPUFactory_2/GPUFactory_2/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     public static void Main(string[] args)
     {
+        AsusManufacturer asusManufacturer = new AsusManufacturer();
+        asusManufacturer.AssembleGpu();
+        
+        MsiManufacturer msiManufacturer = new MsiManufacturer();
+        msiManufacturer.AssembleGpu();
 
     }
 }
