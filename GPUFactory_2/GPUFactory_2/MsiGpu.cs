adb397bce156fe88313a818bf4986e4d6af72553
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-14 21:59:06 +0530

    Create MsiGpu.cs

---
+++ GPUFactory_2/GPUFactory_2/MsiGpu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPUFactory_2
+{
+  public class MsiGpu:Gpu
+    {
+        public void Assemble() {
+            Console.WriteLine("MSI GPU Assemble...");       
+        }
+
+    }
+}
