b9f655fc5707489369859824247a6a66f1d69575
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-20 11:09:11 +0530

    Update Program.cs

--- Singleton_Pattern/Singleton_Pattern/Program.cs
+++ Singleton_Pattern/Singleton_Pattern/Program.cs
@@ -1,7 +1,10 @@
-﻿class Program
+﻿using Singleton_Pattern;
+
+class Program
 {
     public static void Main(string[] args)
     {
-
+        WashingMachine washingMachine = WashingMachine.GetInstance();
+        washingMachine.ShowMessage();
     }
 }
