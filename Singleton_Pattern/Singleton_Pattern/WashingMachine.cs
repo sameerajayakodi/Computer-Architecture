b528cdae24a9b821b8c26bd7a1f604ab6e6b3148
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-20 11:06:51 +0530

    Create WashingMachine.cs

---
+++ Singleton_Pattern/Singleton_Pattern/WashingMachine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    public class WashingMachine
+    {
+        private WashingMachine() { }
+
+        private static WashingMachine instance = new WashingMachine();
+
+        public static WashingMachine GetInstance()
+        {
+            return instance;
+        }
+
+        public void ShowMessage() {
+            Console.WriteLine("I am Singleton object");
+        }
+        
+            }
+    }
+}
