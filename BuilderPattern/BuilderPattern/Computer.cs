1f6af9f28c83ae2614781941dac8a369e5265b76
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-28 00:00:35 +0530

    Update Computer.cs

--- BuilderPattern/BuilderPattern/Computer.cs
+++ BuilderPattern/BuilderPattern/Computer.cs
@@ -8,5 +8,14 @@ namespace BuilderPattern
 {
     public class Computer
     {
+        public string CPU {  get; set; }
+        public string GPU { get; set; }
+        public int RAM {  get; set; }
+        public int Storage { get; set; }
+
+        public override string ToString()
+        {
+            return $"CPU: {CPU}, GPU: {GPU}, RAM: {RAM}GB, Storage: {Storage}GB";
+        }
     }
 }
