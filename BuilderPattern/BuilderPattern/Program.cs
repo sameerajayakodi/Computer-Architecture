01fdcd0179742ecc9da71ee579e30d51b272cde4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-29 10:00:03 +0530

    Update Program.cs

--- BuilderPattern/BuilderPattern/Program.cs
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     public static void Main(string[] args)
     {
-
+        ComputerBuilder builder = new GamingComputerBuilder();
+        Director director = new Director(builder);
+        director.ConstructComputer();
+        Computer gamingComputer = builder.GetComputer();
+        Console.WriteLine(gamingComputer.ToString());
     }
 }
