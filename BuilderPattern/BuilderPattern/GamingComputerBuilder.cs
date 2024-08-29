2bc36b23058edc3c1dedcbf833b4bb6529b7d980
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-29 08:56:47 +0530

    Update GamingComputerBuilder.cs

--- BuilderPattern/BuilderPattern/GamingComputerBuilder.cs
+++ BuilderPattern/BuilderPattern/GamingComputerBuilder.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace BuilderPattern
 {
-    class GamingComputerBuilder:ComputerBuilder
+    public class GamingComputerBuilder:ComputerBuilder
     {
         private Computer computer = new Computer();
-        void BuildCpu() { computer.CPU = "Intel Core i9";}
-        void BuildGpu() { computer.GPU = "NVIDIA RTX 3080"; }
-        void BuildStorage() { computer.Storage = 1000; }
-        void BuildRam() { computer.RAM = 32; }
+        public void BuildCpu() { computer.CPU = "Intel Core i9";}
+        public void BuildGpu() { computer.GPU = "NVIDIA RTX 3080"; }
+        public void BuildStorage() { computer.Storage = 1000; }
+        public void BuildRam() { computer.RAM = 32; }
 
-        Computer GetComputer()
+        public Computer GetComputer()
         {
             return computer;
         }
