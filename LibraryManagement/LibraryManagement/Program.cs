330c5a576ab6c02d572bbdf52f4ca007fb12ba4f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-10 13:36:27 +0530

    Create Program.cs

---
+++ LibraryManagement/LibraryManagement/Program.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        LibraryManager library = LibraryManager.GetInstance();
+
+        
+        library.AddBook("Madolduwa");
+        library.AddBook("Maths");
+
+        
+        library.AddUser("Sameera");
+        library.AddUser("Jayakodi");
+    }
+}
