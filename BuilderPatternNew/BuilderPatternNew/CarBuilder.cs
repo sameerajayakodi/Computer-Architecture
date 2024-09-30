8bccb3a31ae129ffd6c4dfc24dfe2c65e26ceae0
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-30 19:30:23 +0530

    Create CarBuilder.cs

---
+++ BuilderPatternNew/BuilderPatternNew/CarBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternNew
+{
+    public interface CarBuilder
+    {
+        public void BuildId();
+        public void BuildBrand();
+        public void BuildModel();
+        public void BuildColor();
+        public Car Build();
+    }
+}
