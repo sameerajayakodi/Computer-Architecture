b4500690f9cd5ddd2d0c8f354d6512633422cc9d
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-30 19:30:25 +0530

    Create Director.cs

---
+++ BuilderPatternNew/BuilderPatternNew/Director.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternNew
+{
+    public class Director
+    {
+        CarBuilder carBuilder;
+
+        public Director(CarBuilder carBuilder)
+        {
+            this.carBuilder = carBuilder;
+        }
+
+        public Car Construct()
+        {
+            carBuilder.BuildId();
+            carBuilder.BuildBrand();
+            carBuilder.BuildModel();
+            carBuilder.BuildColor();
+
+            return carBuilder.Build();
+        }
+    }
+}
