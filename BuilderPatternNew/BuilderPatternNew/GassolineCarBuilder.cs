5ac8201562db45c9e28c9f438f497998a2bfa118
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-30 19:30:30 +0530

    Create GassolineCarBuilder.cs

---
+++ BuilderPatternNew/BuilderPatternNew/GassolineCarBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternNew
+{
+    public class GassolineCarBuilder:CarBuilder
+    {
+        Car car;
+
+        public GassolineCarBuilder()
+        {
+            car = new Car();
+        }
+
+        public void BuildId()
+        {
+            car.setId(2);
+        }
+
+        public void BuildBrand()
+        {
+            car.setBrand("Ford");
+        }
+
+        public void BuildModel()
+        {
+            car.setModel("Mustang");
+        }
+        
+        public void BuildColor()
+        {
+            car.setColor("Blue");
+        }
+        public Car Build() 
+        {
+            return car;
+        }
+    }
+}
