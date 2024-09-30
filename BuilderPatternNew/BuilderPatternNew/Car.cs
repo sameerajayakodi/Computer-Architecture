62e4302d4b9db0b98ea2396cc9827b7594479554
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-30 19:30:21 +0530

    Create Car.cs

---
+++ BuilderPatternNew/BuilderPatternNew/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternNew
+{
+    public class Car
+    {
+        private int id;
+        private string? brand;
+        private string? model;
+        private string? color;
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public void setBrand(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public void setModel(string model)
+        {
+            this.model = model;
+        }
+
+        public void setColor(string color) 
+        { 
+            this.color = color; 
+        }
+
+
+       public void Display()
+        {
+            Console.WriteLine("Car ID: " + id);
+            Console.WriteLine("Car Brand: " + brand);
+            Console.WriteLine("Car Model: " + model);
+            Console.WriteLine("Car Color: " + color);
+        }
+    }
+}
