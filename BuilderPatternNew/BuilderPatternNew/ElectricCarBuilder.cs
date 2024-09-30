241ce4713ee8d2e29a2bd7032612321d013b902c
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-30 19:30:27 +0530

    Create ElectricCarBuilder.cs

---
+++ BuilderPatternNew/BuilderPatternNew/ElectricCarBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternNew
+{
+    public class ElectricCarBuilder: CarBuilder
+    {
+        Car car;
+        public ElectricCarBuilder()
+        {
+            car = new Car();
+        }
+
+        public void BuildId()
+        {
+            car.setId(1);
+        }
+
+        public void BuildBrand()
+        {
+            car.setBrand("Tesla");
+        }
+
+        public void BuildModel()
+        {
+            car.setModel("Model S");
+        }
+
+        public void BuildColor()
+        {
+            car.setColor("Red");
+        }
+
+        public Car Build()
+        {
+            return car;
+        }
+    }
+}
