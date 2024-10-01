1b3fd512bd0af84dc05f6502388a5a4cebbc315e
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-01 21:31:22 +0530

    Create Computer.cs

---
+++ ComputerBuilder/ComputerBuilder/Computer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerBuilder
+{
+    public class Computer
+    {
+       // cpu , ram , storage
+
+        private string cpu;
+        private string ram;
+        private string storage;
+
+
+        public void setCpu(string cpu)
+        {
+            this.cpu = cpu;
+        }
+
+        public void setRam(string ram)
+        {
+            this.ram = ram;
+        }
+
+        public void setStorage(string storage)
+        {
+            this.storage = storage;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("CPU: " + cpu);
+            Console.WriteLine("RAM: " + ram);
+            Console.WriteLine("Storage: " + storage);
+        }
+    }
+}
