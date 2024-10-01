6b62d8cd14f2a71084e17f97560765fd88baf171
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-01 21:31:35 +0530

    Create GamingComputer.cs

---
+++ ComputerBuilder/ComputerBuilder/GamingComputer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerBuilder
+{
+    public class GamingComputer: ComputerBuilder
+    {
+        Computer computer;
+
+        public GamingComputer()
+        {
+            computer = new Computer();
+        }
+
+        public void BuildCpu()
+        {
+            computer.setCpu("i9");
+        }
+
+        public void BuildRam()
+        {
+            computer.setRam("32GB");
+        }
+
+        public void BuildStorage()
+        {
+            computer.setStorage("1TB SSD");
+        }
+
+        public Computer Build()
+        {
+            return computer;
+        }
+    }
+}
