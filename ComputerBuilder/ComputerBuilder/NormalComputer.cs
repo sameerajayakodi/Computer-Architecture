a6eb354b067452770db4d7b01e5e54e0279fe7d2
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-01 21:31:39 +0530

    Create NormalComputer.cs

---
+++ ComputerBuilder/ComputerBuilder/NormalComputer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerBuilder
+{
+    public class NormalComputer:ComputerBuilder
+    {
+        Computer computer;
+
+        public NormalComputer()
+        {
+            computer = new Computer();
+        }
+
+        public void BuildCpu()
+        {
+            computer.setCpu("i5");
+        }
+
+        public void BuildRam()
+        {
+            computer.setRam("16GB");
+        }
+
+        public void BuildStorage()
+        {
+            computer.setStorage("512GB SSD");
+        }
+
+        public Computer Build()
+        {
+            return computer;
+        }
+    }
+}
