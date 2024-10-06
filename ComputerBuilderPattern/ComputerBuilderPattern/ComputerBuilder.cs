0d935d826e20e877ded2a333c5e2024367ee922c
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-06 21:20:51 +0530

    Create ComputerBuilder.cs

---
+++ ComputerBuilderPattern/ComputerBuilderPattern/ComputerBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerBuilderPattern
+{
+    public interface ComputerBuilder
+    { 
+        public void BuildProcessor();
+        public void BuildRam();
+        public void BuildStorage();
+        public void BuildGraphicCard();
+
+        public void BuildPowerSupply();
+        public void BuildOs();
+        
+        public Computer Build();
+    }
+}
