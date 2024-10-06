79a8cd3eaaa77fda7da69f72e1c50302cdff82c3
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-06 21:21:56 +0530

    Create WorkComputerBuilder.cs

---
+++ ComputerBuilderPattern/ComputerBuilderPattern/WorkComputerBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerBuilderPattern
+{
+    public class WorkComputerBuilder : ComputerBuilder
+    {
+        Computer computer;
+
+        public WorkComputerBuilder()
+        {
+            computer = new Computer();
+        }
+
+        public void BuildProcessor()
+        {
+            computer.setProcessor("Intel Core i5-1100H");
+        }
+
+        public void BuildRam()
+        {
+            computer.setRam("Samsung 16GB");
+        }
+
+        public void BuildStorage()
+        {
+            computer.setStorage("Samsung 500GB");
+        }
+
+        public void BuildGraphicCard()
+        {
+            computer.setGraphicsCard("Nvidia GeForce GTX 1660");
+        }
+
+        public void BuildPowerSupply()
+        {
+            computer.setPowerSupply("Corsair 650");
+        }
+
+        public void BuildOs()
+        {
+            computer.setOS("Windows 10 Home");
+        }
+
+        public Computer Build()
+        {
+            return computer;
+        }
+    }
+}
