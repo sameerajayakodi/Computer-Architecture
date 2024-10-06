19c6147bd131bf684aa1911bb895a3e2e83125f0
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-06 21:21:53 +0530

    Create Program.cs

---
+++ ComputerBuilderPattern/ComputerBuilderPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ComputerBuilderPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Computer gamingComputer = new Director(new GamingComputerBuilder()).Construct();
+        gamingComputer.Display();
+
+        Console.WriteLine("------------------------------------------------------------");
+        Computer workingComputer = new Director(new WorkComputerBuilder()).Construct();
+        workingComputer.Display();
+    }
+}
