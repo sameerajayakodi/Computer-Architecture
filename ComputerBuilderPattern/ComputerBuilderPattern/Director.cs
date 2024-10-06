28320da040e62f6b38f1bcbdc21289daeec44bd4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-06 21:21:00 +0530

    Create Director.cs

---
+++ ComputerBuilderPattern/ComputerBuilderPattern/Director.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerBuilderPattern
+{
+    public class Director
+    {
+        ComputerBuilder computerBuilder;
+
+        public Director(ComputerBuilder computerBuilder)
+        {
+            this.computerBuilder = computerBuilder;
+        }
+
+
+        public Computer Construct()
+        {
+            computerBuilder.BuildProcessor();
+            computerBuilder.BuildRam();
+            computerBuilder.BuildStorage();
+            computerBuilder.BuildGraphicCard();
+            computerBuilder.BuildPowerSupply();
+            computerBuilder.BuildOs();
+            return computerBuilder.Build();
+        }
+    }
+}
