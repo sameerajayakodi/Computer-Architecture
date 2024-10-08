7ba12a4afa131bbfe0f3e53a9c2d29cd1518c585
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-08 15:51:57 +0530

    Create Circle.cs

---
+++ PrototypeDesignPattern/PrototypeDesignPattern/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDesignPattern
+{
+    public class Circle: ShapePrototype
+    {
+        private string color;
+        public Circle(string color)
+        {
+            this.color = color;
+        }
+
+        public ShapePrototype Clone()
+        {
+            return new Circle(this.color);
+        }
+
+        public void Drow()
+        {
+            Console.WriteLine("Drowing "+color+" Color Circle." );
+        }
+    }
+}
