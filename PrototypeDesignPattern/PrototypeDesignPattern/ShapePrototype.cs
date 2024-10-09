4438fbb9260c61c9580616e921dd95bc7a68171b
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-09 11:44:25 +0530

    Create ShapePrototype.cs

---
+++ PrototypeDesignPattern/PrototypeDesignPattern/ShapePrototype.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDesignPattern
+{
+    public interface ShapePrototype
+    {
+        ShapePrototype Clone();
+        void Drow();
+    }
+}
