2a8015f236e0fbb995555fbe65f602d00324bb2a
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-09 11:44:21 +0530

    Create Program.cs

---
+++ PrototypeDesignPattern/PrototypeDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PrototypeDesignPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Circle circlePrototype = new Circle("Red");
+        Circle circle1 = (Circle)circlePrototype.Clone();
+        circle1.Drow();
+        circlePrototype.Drow();
+    }
+}
