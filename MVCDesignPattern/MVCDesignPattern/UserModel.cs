092a36eeed55325175a93f5894ecc8b5eaaf3df4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-12 19:34:09 +0530

    Create UserModel.cs

---
+++ MVCDesignPattern/MVCDesignPattern/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDesignPattern
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public UserModel(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
