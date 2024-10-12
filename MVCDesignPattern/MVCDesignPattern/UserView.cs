8c0bdbd852fd8fb544bdda2dcbf790535d1d9863
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-12 19:34:12 +0530

    Create UserView.cs

---
+++ MVCDesignPattern/MVCDesignPattern/UserView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDesignPattern
+{
+    public class UserView
+    {
+        public void DisplayUserDetails(string userName, int userAge)
+        {
+            Console.WriteLine("User Details:");
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Age: {userAge}");
+        }
+
+        public string GetUserInput(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
