e7b0e707d478012721b9356ce5aa49ab1a930fdb
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-12 19:34:10 +0530

    Create UserController.cs

---
+++ MVCDesignPattern/MVCDesignPattern/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDesignPattern
+{
+    public class UserController
+    {
+        private UserModel _model;
+        private UserView _view;
+
+        public UserController(UserModel model, UserView view)
+        {
+            _model = model;
+            _view = view;
+        }
+
+        public void UpdateView()
+        {
+            _view.DisplayUserDetails(_model.Name, _model.Age);
+        }
+
+        public void UpdateModelFromInput()
+        {
+            _model.Name = _view.GetUserInput("Enter Name: ");
+            _model.Age = int.Parse(_view.GetUserInput("Enter Age: "));
+        }
+    }
+}
