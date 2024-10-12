cf4e086985a13b381e53e720995ac7c84f3c0c5b
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-12 19:34:07 +0530

    Update Program.cs

--- MVCDesignPattern/MVCDesignPattern/Program.cs
+++ MVCDesignPattern/MVCDesignPattern/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MVCDesignPattern;
+
+class Program
+{
+    public static void Main()
+    {
+        UserModel model = new UserModel("John Doe", 30);
+
+        // Create the view
+        UserView view = new UserView();
+
+        // Create the controller
+        UserController controller = new UserController(model, view);
+
+        // Display initial data
+        controller.UpdateView();
+
+        // Update model from user input and display updated data
+        controller.UpdateModelFromInput();
+        controller.UpdateView();
+    }
+}
