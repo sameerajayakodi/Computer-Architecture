173e0e1aba59adf34a95f9d800ca01a6d084ef9a
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-23 16:32:46 +0530

    Create TodoController.cs

---
+++ Asp.net/Asp.net/Controllers/TodoController.cs
@@ -0,0 +1,34 @@
+﻿using Asp.net.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Asp.net.Models;
+
+namespace Asp.net.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        
+        private TodoServicers _todoServicers; 
+
+        public TodoController()
+        {
+            _todoServicers = new TodoServicers();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTodos()
+        {
+            return Ok(_todoServicers.AllTodos());
+        }
+
+        [HttpPost]
+        public IActionResult AddTodo([FromBody] Todo todo)
+        {
+            _todoServicers.AddTodos(todo);
+            return Ok(_todoServicers.AddTodos);
+        }
+        
+    }
+}
