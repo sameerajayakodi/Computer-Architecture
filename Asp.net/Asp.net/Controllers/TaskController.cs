a010adab38833690b6c8c0e7ae4a104effcf4104
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-23 16:32:44 +0530

    Create TaskController.cs

---
+++ Asp.net/Asp.net/Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.net.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpPost]
+        public string[] Tasks() { 
+          return new string[] { "Task 1", "Task 2", "Task 3" };
+        }
+
+
+        [HttpGet]
+        public IActionResult AddTasks()
+        {
+            var error = false;
+            if (error) {                            
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
