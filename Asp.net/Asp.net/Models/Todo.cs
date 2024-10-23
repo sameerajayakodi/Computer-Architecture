7847b377f163c9f1078204023461dc6dfccc3013
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-23 16:32:48 +0530

    Create Todo.cs

---
+++ Asp.net/Asp.net/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace Asp.net.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Due { get; set; }
+        public string Status { get; set; }
+    }
+}
